d438e35bd648b71a7ba70b7ac307b3471936dbe4
SaiPreethi Induri <Saipreethi_Induri@epam.com>
2020-02-20 11:57:26 +0530

    Add project files.

---
+++ sqlinjection/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="sqlinjection.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 127px;
+            margin-right: 181px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" CssClass="auto-style1" OnClick="Button1_Click" Text="getproducts" Width="107px" />
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical">
+            <AlternatingRowStyle BackColor="#DCDCDC" />
+            <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+            <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EEEEEE" ForeColor="Black" />
+            <SelectedRowStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#0000A9" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#000065" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ sqlinjection/WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace sqlinjection
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //DataTable dt = new DataTable();
+            //using (SqlConnection sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString))
+            //{
+            //    string sql = "SELECT * FROM productinventory WHERE productname = @SearchText";
+            //    using (SqlCommand sqlCmd = new SqlCommand(sql, sqlConn))
+            //    {
+            //        sqlCmd.Parameters.AddWithValue("@SearchText", TextBox1.Text);
+            //        sqlConn.Open();
+
+            //        using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCmd))
+            //        {
+            //            sqlAdapter.Fill(dt);
+            //        }
+            //    }
+            //}
+
+            //if (dt.Rows.Count > 0)
+            //{
+            //    GridView1.DataSource = dt;
+            //    GridView1.DataBind();
+            //}
+            SqlConnection conn = new SqlConnection(@"Data Source=EPINHYDW008B\MSSQLSERVER1;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from productinventory where productname = '" + TextBox1.Text + "'", conn);
+            conn.Open();
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+
+            conn.Close();
+
+
+        }
+    }
+}
